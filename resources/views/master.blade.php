05756fc0ddcf19dfac220cdc9794b10ecbf17a36
Abdelrahman Gamal <abdelrahman_nasser@hotmail.com>
2018-08-06 17:33:30 +0200

    Add_user

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<<html lang="en">
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,7 +11,7 @@
 <body>
 
 <div class="container">
-	
+	@yield('content')
 </div>
 </body>
 </html>
