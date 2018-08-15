8ae9bc0381506541808b61791f77f00e155cc8ea
hamidfawzi97 <35073262+hamidfawzi97@users.noreply.github.com>
2018-08-15 20:10:50 +0200

    asdasd

--- app/Exports/SalaryExport.php
+++ app/Exports/SalaryExport.php
@@ -27,7 +27,7 @@ class SalaryExport implements FromCollection
     	if($this->from == '' or $this->to == ''){
             $salaries_array = Salary::all();
         }else{
-            $salaries_array = Salary::whereBetween('delivery_date', [$this->from." 00:00:00", $this->to." 00:00:00"])->orWhereDate('created_at',$this->to." 00:00:00")->get();
+            $salaries_array = Salary::whereBetween('delivery_date', [$this->from, $this->to)->orWhereDate('delivery_date',$this->to)->get();
         }
     	
     	foreach ($salaries_array as $value) {
